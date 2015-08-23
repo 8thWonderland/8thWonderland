30d3c858cf37514b6c7fea6428d02f9f2140666f
Axel Venet <kern046@gmail.com>
2015-08-23 17:23:26 +0200

    Add path constants waiting for replacement

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once 'vendor/autoload.php';
 use Wonderland\Library\Application;
 
 define('PUBLIC_ROOT', 'http://127.0.0.1/Developpement/PHP/8thWonderland/site_23-04-2012/src/8thWonderland/');
+define ( 'VIEWS_PATH', PUBLIC_ROOT . 'Application/views/' );
 define ( 'CSS_PATH', PUBLIC_ROOT . 'public/css/' );
 define ( 'IMG_PATH', PUBLIC_ROOT . 'public/images/' );
 define ( 'ICO_PATH', PUBLIC_ROOT . 'public/icones/' );
