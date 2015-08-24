e13974f621a5d041435849deaee283eba009460b
Axel Venet <kern046@gmail.com>
2015-08-24 08:08:16 +0200

    Refactor poll logic and method names

--- src/8thWonderland/Application/Model/Poll.php
+++ src/8thWonderland/Application/Model/Poll.php
@@ -15,32 +15,41 @@ use Wonderland\Library\Auth;
  */
 class Poll
 {
-    // Affichage des motions en cours
-    // ==============================
-    public function display_motionsinprogress()
+    /**
+     * Return motions which are currently being voted
+     * 
+     * @TODO: Return only data without any notion of view
+     * 
+     * @return string
+     */
+    public function displayActiveMotions()
     {
-        $db = Registry::get('db');
         $translate = Registry::get('translate');
         $auth = Auth::getInstance();
         $id_member = $auth->getIdentity();
-        $reponse = '';
         
-        $req = "SELECT motion_id, title_key, date_fin_vote " .
-               "FROM Motions " .
-               "WHERE Date_fin_vote >  NOW() " .
-               "ORDER BY date_fin_vote DESC ";
-        
-        $list_motions = $db->query($req);
+        $list_motions = Registry::get('db')->query(
+            'SELECT motion_id, title_key, date_fin_vote ' .
+            'FROM Motions ' .
+            'WHERE Date_fin_vote >  NOW() ' .
+            'ORDER BY date_fin_vote DESC '
+        );
         if ($list_motions->num_rows > 0) {
+            $response = '';
             while ($motion = $list_motions->fetch_assoc()) {
-                $reponse .= "<tr><td><a onclick=\"Clic('/Motion/display_motion', 'motion_id=" . $motion['motion_id'] . "', 'milieu_milieu'); return false;\">" . $motion['title_key'] . "</a></td>" .
-                            "<td><a onclick=\"Clic('/Motion/display_motion', 'motion_id=" . $motion['motion_id'] . "', 'milieu_milieu'); return false;\">" . $motion['date_fin_vote'] . "</a></td>";
-                if ($this->poll_active($motion['motion_id'], $id_member) == 0) {
-                    $reponse .= "<td><div class='bouton'><a onclick=\"Clic('/Motion/display_vote', 'motion_id=" . $motion['motion_id'] . "', 'milieu_milieu'); return false;\">" .
-                                "<span style='color: #dfdfdf;'>" . $translate->translate('btn_votemotion') . "</span></a></div></td>";
+                $response .=
+                    "<tr><td><a onclick=\"Clic('/Motion/display_motion', 'motion_id={$motion['motion_id']}', 'milieu_milieu'); return false;\">{$motion['title_key']}</a></td>" .
+                    "<td><a onclick=\"Clic('/Motion/display_motion', 'motion_id={$motion['motion_id']}', 'milieu_milieu'); return false;\">{$motion['date_fin_vote']}</a></td>"
+                ;
+                if ($this->hasAlreadyVoted($motion['motion_id'], $memberId) == 0) {
+                    $response .=
+                        "<td><div class='bouton'><a onclick=\"Clic('/Motion/display_vote', 'motion_id={$motion['motion_id']}', 'milieu_milieu'); return false;\">" .
+                        "<span style='color: #dfdfdf;'>{$translate->translate('btn_votemotion')}</span></a></div></td>"
+                    ;
                 }
-                $reponse .= "</tr>";
+                $response .= '</tr>';
             }
+            return $response;
         }
         else {
             $reponse = "<tr><td>" . $translate->translate('no_result') . "</td></tr>";
@@ -49,31 +58,27 @@ class Poll
         return $reponse;
     }
     
-    
-    // Affichage des motions archivées
-    // ===============================
-    public function display_motions()
-    {
-        $db = Registry::get('db');
-
-        $req = "SELECT Motion_id, Title_key, Label_key, Submission_date, Date_fin_vote, Citizen_id " .
-               "FROM Motions, Motions_Themes " .
-               "WHERE Date_fin_vote < NOW() AND Motions.Theme_id=Motions_Themes.Theme_id " .
-               "ORDER BY motion_id DESC";
-
-        return $db->select($req);
+    /**
+     * @return array
+     */
+    public function displayMotions() {
+        return Registry::get('db')->select(
+            'SELECT Motion_id, Title_key, Label_key, Submission_date, Date_fin_vote, Citizen_id ' .
+            'FROM Motions, Motions_Themes ' .
+            'WHERE Date_fin_vote < NOW() AND Motions.Theme_id=Motions_Themes.Theme_id ' .
+            'ORDER BY motion_id DESC'
+        );
     }
-    
     
     // Récupération des détails d'une motion
     // =====================================
-    public function display_detailsmotion($id)
+    public function displayMotionDetails($id)
     {
-        $db = Registry::get('db');
-        $req = "SELECT motion_id, title_key, label_key, description, moyens, submission_date, date_fin_vote " .
-           "FROM Motions, Motions_Themes " .
-           "WHERE motion_id = " . $id . " AND Motions.theme_id=Motions_Themes.Theme_id";
-        $motion = $db->select($req);
+        $motion = Registry::get('db')->select(
+            'SELECT motion_id, title_key, label_key, description, moyens, submission_date, date_fin_vote ' .
+            'FROM Motions, Motions_Themes ' .
+            "WHERE motion_id = $id AND Motions.theme_id=Motions_Themes.Theme_id"
+        );
         $motion[0]['vote'] = $this->_getVotes($id);
         return $motion;
     }
@@ -83,11 +88,9 @@ class Poll
     // ==================================
     public static function _getThemes()
     {
-        $db = Registry::get('db');
-        $req = "SELECT label_key " .
-               "FROM Motions_Themes " .
-               "ORDER BY label_key ASC";
-        return $db->query($req);
+        return Registry::get('db')->query(
+            'SELECT label_key FROM Motions_Themes ORDER BY label_key ASC'
+        );
     }
     
     
@@ -96,73 +99,86 @@ class Poll
     public static function _getVotes($id_motion)
     {
         $db = Registry::get('db');
-        $approved = $db->count("Motions_Votes", " WHERE Motion_id=" . $id_motion . " AND Choix=1");
-        $refused = $db->count("Motions_Votes", " WHERE Motion_id=" . $id_motion . " AND Choix=2");
-        return array($approved, $refused);
+        return [
+            $db->count('Motions_Votes', " WHERE Motion_id=$id_motion AND Choix=1"),
+            $db->count('Motions_Votes', " WHERE Motion_id=$id_motion AND Choix=2")
+        ];
     }
     
-    
-    // Valide le dépot d'une motion
-    // ============================
-    public function valid_motion($p_title, $p_theme, $p_description, $p_means)
+    /**
+     * 
+     * @param type $p_title
+     * @param type $p_theme
+     * @param type $p_description
+     * @param type $p_means
+     * @return type
+     */
+    public function validMotion($p_title, $p_theme, $p_description, $p_means)
     {
-        $db     = Registry::get('db');
         $auth   = Auth::getInstance();
         
         $theme = htmlentities(utf8_decode($p_theme), ENT_QUOTES);
         $title = htmlentities(utf8_decode($p_title), ENT_QUOTES);
-        $description = htmlentities($p_description);
-        $means = htmlentities($p_means);
+        $description = nl2br(htmlentities($p_description));
+        $means = nl2br(htmlentities($p_means));
         $author = $auth->getIdentity();
                 
-        $req = "INSERT INTO Motions " .
-               "(Theme_id, Title_key, Description, Moyens, Submission_date, Date_fin_vote, Citizen_id) " .
-               "values ('" . $theme . "', '" . $title . "', '" . nl2br($description) . "', '" . nl2br($means) . "',  NOW(), " .
-               "DATE_ADD(NOW(), INTERVAL (SELECT Duree FROM Motions_Themes WHERE Motions_Themes.Theme_id = " . $theme . ") DAY), " .
-               $author . ")";
-        return $db->query($req);
+        return Registry::get('db')->query(
+            "INSERT INTO Motions " .
+            "(Theme_id, Title_key, Description, Moyens, Submission_date, Date_fin_vote, Citizen_id) " .
+            "values ('" . $theme . "', '" . $title . "', '" . $description . "', '" . $means . "',  NOW(), " .
+            "DATE_ADD(NOW(), INTERVAL (SELECT Duree FROM Motions_Themes WHERE Motions_Themes.Theme_id = " . $theme . ") DAY), " .
+            $author . ")"
+        );
     }
     
-    
-    // Enregistre le vote d'un membre
-    // ==============================
-    public function vote_motion($id, $vote)
+    /**
+     * Returns number of affected rows
+     * 
+     * @param int $id
+     * @param string $vote
+     * @return int
+     */
+    public function voteMotion($id, $vote)
     {
         $db     = Registry::get('db');
         $auth   = Auth::getInstance();
         $member = $auth->getIdentity();
         $date   = date('Y-m-d h-i-s');
-        (isset($_SERVER['REMOTE_ADDR']))?$ip = $_SERVER['REMOTE_ADDR']:$ip='inconnue';
+        $ip =
+            (isset($_SERVER['REMOTE_ADDR']))
+            ? $_SERVER['REMOTE_ADDR']
+            : 'inconnue'
+        ;
         
-        // Enregistrement du jeton de vote
-        $req = "INSERT INTO Motions_Votes_Jetons " .
-               "(Motion_id, Citizen_id, Date, Ip) " .
-               "VALUES (" . $id . ", " . $member . ", '" . $date . "', '" . $ip . "')";
-        $db->query($req);
+        $db->query(
+            'INSERT INTO Motions_Votes_Jetons ' .
+            '(Motion_id, Citizen_id, Date, Ip) ' .
+            "VALUES ($id, $member, '$date', '$ip')"
+        );
         if ($db->affected_rows == 0)    {   return $db->affected_rows;  }
         
-        $choix = ($vote=='approved')?1:2;
+        $choice = ($vote === 'approved') ? 1 : 2;
         
-        // Enregistrement du résultat de vote
-        $hash = hash("sha512",$db->insert_id."#".$id."#".$member."#".$choix."#".$date."#".$ip);
-        $req = "INSERT INTO Motions_Votes " .
-               "(Motion_id, Choix, Hash) " .
-               "VALUES (" . $id . ", '" . $choix . "', '" . $hash . "')";
-        $db->query($req);
+        $hash = hash('sha512', "{$db->insert_id}#$id#$member#$choice#$date#$ip");
+        $db->query(
+            'INSERT INTO Motions_Votes ' .
+            '(Motion_id, Choix, Hash) ' .
+            "VALUES ($id, '$choice', '$hash')"
+        );
         return $db->affected_rows;
     }
     
     
     // Test si le membre a déjà voté la motion
     // =======================================
-    protected function poll_active($id_motion, $id_member)
+    protected function hasAlreadyVoted($id_motion, $id_member)
     {
-        $db     = Registry::get('db');
-        return $db->count("Motions_Votes_Jetons", " WHERE Motion_id=" . $id_motion . " AND Citizen_id=" . $id_member);
+        return Registry::get('db')->count("Motions_Votes_Jetons", " WHERE Motion_id=" . $id_motion . " AND Citizen_id=" . $id_member);
     }
     
     
-    function check_motion(){
+    function checkMotion(){
         // vérification des votes
         // en commentaire car c'est un code sensible à n'utiliser qu'en cas de crise
     /*
