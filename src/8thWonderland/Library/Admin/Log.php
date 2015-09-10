78c472c86303376cbdb1471af0062c902fa96b8d
Axel Venet <kern046@gmail.com>
2015-09-10 12:22:08 +0200

    Refactor Log class

--- src/8thWonderland/Library/Admin/Log.php
+++ src/8thWonderland/Library/Admin/Log.php
@@ -2,16 +2,9 @@
 
 namespace Wonderland\Library\Admin;
 
-use Wonderland\Library\Memory\Registry;
+use Wonderland\Library\Application;
 
-/**
- * class admin_logs
- *
- * Mise en place d'un système de log
- *
- */
-class Log
-{
+class Log {
     const EMERG   = 0;  // Emergency: system is unusable
     const ALERT   = 1;  // Alert: action must be taken immediately
     const CRIT    = 2;  // Critical: critical conditions
@@ -20,52 +13,51 @@ class Log
     const NOTICE  = 5;  // Notice: normal but significant condition
     const INFO    = 6;  // Informational: informational messages
     const DEBUG   = 7;  // Debug: debug messages
+    /** @var \Wonderland\Library\Application **/
+    protected $application;
+    /** @var array **/
+    protected $priorities = [];
+    /** @var array **/
+    protected $writers = ['DB', 'MAIL', 'FILE'];
+    /** @var string **/
+    protected $writer;
     
-    protected $_priorities = array();                                           // Tableau des constantes
-    protected $_writers = array(0 => "DB", 1=> "MAIL", 2 => "FILE");            // Type de writers possibles
-    protected $writer;                                                          // writer choisi pour cette instance
-    
-    
-    public function __construct($writer)
-    {
-        $r = new \ReflectionClass($this);
-        $this->_priorities = array_flip($r->getConstants());
-        if (in_array(strtoupper($writer), $this->_writers))     {   $this->writer = strtoupper($writer);    }
+    /**
+     * @param Application $application
+     */
+    public function __construct(Application $application) {
+        $this->application = $application;
     }
     
+    /**
+     * @param string $writer
+     */
+    public function setWriter($writer) {
+        $this->priorities = array_flip((new \ReflectionClass($this))->getConstants());
+        if (in_array(strtoupper($writer), $this->writers)) {
+            $this->writer = strtoupper($writer);
+        }
+    }
     
-    // Enregistrement d'un log d'event
-    // ===============================
-    public function log($message, $priority)
-    {       
-        switch ($this->writer)
-        {
-            case "DB":
-                $db = Registry::get('db');
-                $db->query("INSERT INTO logs (level, msg) VALUES (" . $priority . ", '" . $db->real_escape_string($message) . "')");
-                break;
-
-            case "MAIL":
-                break;
-
-            case "FILE":
-                break;
+    /**
+     * @param string $message
+     * @param int $priority
+     */
+    public function log($message, $priority) {       
+        if ($this->writer === 'DB') {
+            $db = $this->application->get('mysqli');
+            $db->query(
+                "INSERT INTO logs (level, msg) VALUES ($priority, '{$db->real_escape_string($message)}')"
+            );
         }
     }
         
-    
-    // Affichage des motions archivées
-    // ===============================
-    public static function display_dblogs()
-    {
-        $db = Registry::get('db');
-
-        $req = "SELECT level, timelogs, msg " .
-               "FROM logs " .
-               "ORDER BY timelogs DESC";
-
-        return $db->select($req);
+    /**
+     * @return array
+     */
+    public static function displayDbLogs() {
+        return $this->application->get('mysqli')->select(
+            'SELECT level, timelogs, msg FROM logs ORDER BY timelogs DESC'
+        );
     }
 }
-
-?>
