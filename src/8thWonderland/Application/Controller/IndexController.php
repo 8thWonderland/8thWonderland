e007432528d2d69d7495280366f8d72a3e582ff6
Axel Venet <kern046@gmail.com>
2015-09-10 17:43:48 +0200

    Refactor IndexController

--- src/8thWonderland/Application/Controller/IndexController.php
+++ src/8thWonderland/Application/Controller/IndexController.php
@@ -4,41 +4,33 @@ namespace Wonderland\Application\Controller;
 
 use Wonderland\Library\Controller\ActionController;
 
-use Wonderland\Library\Auth;
-
-use Wonderland\Library\Memory\Registry;
-
 class IndexController extends ActionController {   
-    public function indexAction()
-    {
-        global $application;
+    public function indexAction() {
         // controle si l'utilisateur est déjà connecté
-        if (Auth::hasIdentity()) {
-            $this->redirect("Intranet/index");
+        if ($this->application->get('auth')->hasIdentity()) {
+            $this->redirect('Intranet/index');
         }
 
         $this->viewParameters['appli_status'] = 1;
-        $this->viewParameters['translate'] = Registry::get("translate");
+        $this->viewParameters['translate'] = $this->application->get('translate');
 	$this->viewParameters['msg'] = '';
-        $this->default_file = "{$application->getRootPath()}Application/views/informations/presentation.view";
+        $this->default_file = "{$this->application->getRootPath()}Application/views/informations/presentation.view";
 
-        $this->render("accueil");
-        
+        $this->render('accueil');
     }
 
-
     function presentationAction() {
-        $this->viewParameters['translate'] = Registry::get("translate");
+        $this->viewParameters['translate'] = $this->application->get('translate');
         $this->render('informations/presentation');
     }
 
-
     function subscribeAction() {
-        $translate = Registry::get("translate");
+        $translate = $this->application->get('translate');
         $langs = $translate->getList();
         $sel_lang = '';
-        for ($i=0; $i<count($langs); $i++) {
-            $sel_lang .= "<option value='" . $langs[$i] . "'>" . $translate->translate($langs[$i]) . "</option>";
+        $nbLangs = count($langs);
+        for ($i = 0; $i < $nbLangs; ++$i) {
+            $sel_lang .= "<option value='{$langs[$i]}'>{$translate->translate($langs[$i])}</option>";
         }
         
         $this->viewParameters['translate'] = $translate;
@@ -46,22 +38,21 @@ class IndexController extends ActionController {
         $this->render('members/subscribe');
     }
 
-
     function partnersAction() {
-        $this->viewParameters['translate'] = Registry::get("translate");
+        $this->viewParameters['translate'] = $this->application->get('translate');
         $this->render('informations/partners');
     }
 
 
     function newsAction() {
-        $this->viewParameters['translate'] = Registry::get("translate");
+        $this->viewParameters['translate'] = $this->application->get('translate');
         $this->render('informations/public_news');
     }
 
 
     function contactAction() {
-        $this->viewParameters['translate'] = Registry::get("translate");
-        $this->contact_status = "";
+        $this->viewParameters['translate'] = $this->application->get('translate');
+        $this->contact_status = '';
         $this->render('communications/contactus');
     }
 }
