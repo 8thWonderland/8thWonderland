362ac8d6965d8a1a6b7980b218b5448ae927bedc
Axel Venet <kern046@gmail.com>
2015-09-11 15:42:29 +0200

    Finish the MemberManager and adapt the queries to fit the new structure

--- src/8thWonderland/Application/Manager/MemberManager.php
+++ src/8thWonderland/Application/Manager/MemberManager.php
@@ -4,300 +4,179 @@ namespace Wonderland\Application\Manager;
 
 use Wonderland\Application\Model\Member;
 
+use Wonderland\Library\Application;
+
 class MemberManager {
+    /** @var \Wonderland\Library\Application **/
+    protected $application;
+    
+    /**
+     * @param Application $application
+     */
+    public function __construct(Application $application) {
+        $this->application = $application;
+    }
+    
     /**
      * @param int $id
      * @return \Wonderland\Application\Model\Member
      */
     public function getMember($id) {
-        $data = Registry::get('db')->select(
-            'SELECT Login, Identite, Password, Email, Avatar, Sexe, Pays, Region, '.
-            "DerConnexion, Inscription, isBlocked, Theme, Ip FROM Utilisateurs WHERE IDUser = $id"
+        $data = $this->application->get('mysqli')->select(
+            'SELECT login, password, salt, identity, gender, email, avatar, language, ' .
+            "country, region, last_connected_at, created_at, is_enabled, is_banned, theme FROM users WHERE id = $id"
         )[0];
         return
             (new Member())
-            ->setLogin($data['Login'])
-            ->setIdentity($data['Identite'])
-            ->setPassword($data['Password'])
-            ->setEmail($data['Email'])
-            ->setAvatar($data['Avatar'])
-            ->setGender($data['Sexe'])
-            ->setCountry($data['Pays'])
-            ->setRegion($data['Region'])
-            ->setCreatedAt(new \DateTime($data['Inscription']))
-            ->setLastConnectedAt(new \DateTime($data['DerConnexion']))
-            ->setIsBanned($data['isBlocked'])
+            ->setLogin($data['login'])
+            ->setIdentity($data['identity'])
+            ->setPassword($data['password'])
+            ->setEmail($data['email'])
+            ->setAvatar($data['avatar'])
+            ->setGender($data['gender'])
+            ->setLanguage($data['language'])
+            ->setCountry($data['country'])
+            ->setRegion($data['region'])
+            ->setCreatedAt(new \DateTime($data['created_at']))
+            ->setLastConnectedAt(new \DateTime($data['last_connected_at']))
+            ->setIsEnabled($data['is_enabled'])
+            ->setIsBanned($data['is_banned'])
             ->setTheme($data['Theme'])
         ;
     }
     
-    public function setLogin($value)
-    {
-        $value = htmlentities(trim($value));
-        if ($this->getLogin() != $value) {
-            $db = Registry::get('db');
-            $db->_query(
-                "UPDATE Utilisateurs SET Login='$value' WHERE IDUser={$this->id}"
-            );
-            if ($db->affected_rows == 0) {
-                // log d'échec de mise à jour
-                $db_log = new Log('db');
-                $db_log->log("Echec de l'update du login ({$this->identite})", Log::ERR);
-            }
-            return $db->affected_rows;
-        }
-        return 1;
+    public function createMember(Member $member) {
+        $this->application->get('mysqli')->query(
+            'INSERT INTO Utilisateurs(login, password, salt, identity, gender, ' .
+            'email, avatar, language, country, region, last_connected_at, created_at, is_enabled, is_banned, theme) ' .
+            "VALUES('{$member->getLogin()}', '{$member->getPassword()}', '{$member->getSalt()}', " .
+            "'{$member->getIdentity()}', '{$member->getGender()}', '{$member->getEmail()}', " .
+            "'{$member->getAvatar()}', '{$member->getLanguage()}', '{$member->getCountry()}', " .
+            "'{$member->getRegion()}', '{$member->getLastConnectedAt()->format('c')}', " .
+            "'{$member->getCreatedAt()->format('c')}', {$member->getIsEnabled()}, " .
+            "{$member->getIsBanned()}, '{$member->getTheme()}')"
+        );
     }
     
-    public function setPassword($value)
-    {
-        $value = hash('sha512', $value);
-        if ($this->getPassword() != $value) {
-            $db = Registry::get('db');
-            $db->_query(
-                "UPDATE Utilisateurs SET Password='$value' WHERE IDUser={$this->id}"
-            );
-            if ($db->affected_rows == 0) {
-                // log d'échec de mise à jour
-                $db_log = new Log("db");
-                $db_log->log("Echec de l'update du password ({$this->identite})", Log::ERR);
-            }
-            return $db->affected_rows;
-        }
-        return 1;
-    }
-    
-    public function setIdentite($value)
-    {
-        $value = htmlentities(trim($value));
-        $old_identite = $this->getIdentite();
-        if ($old_identite != $value) {
-            $db = Registry::get('db');
-            if (!self::ctrlIdentity($value)) {   return -1;  }
-            if ($db->count("Utilisateurs", " WHERE Identite='" . $value . "'") > 0) {   return -2;  }
-
-            $req = "UPDATE Utilisateurs SET Identite='" . $value . "' WHERE IDUser=" . $this->id;
-            $db->_query($req);
-            if ($db->affected_rows == 0) {
-                // log d'échec de mise à jour
-                $db_log = new Log("db");
-                $db_log->log("Echec de l'update de l'identite (" . $this->getIdentite() . ")", Log::ERR);
-            } else {
-                $req = "UPDATE phpbb_users SET username='" . $value . "' WHERE username='" . $old_identite . "'";
-                $db->_query($req);
-                if ($db->affected_rows == 0) {
-                    // log d'échec de mise à jour
-                    $db_log = new Log("db");
-                    $db_log->log("Echec de l'update de l'identite du forum (" . $value . ")", Log::ERR);
-                }
-            }
-            return $db->affected_rows;
-        }
-        return 1;
-    }
-    
-    public function setAvatar($value)
-    {
-        if ($this->getAvatar() != $value) {
-            if(!filter_var($value, FILTER_VALIDATE_URL))    {   return -1;  }
-            $db = Registry::get('db');
-            $req = "UPDATE Utilisateurs SET Avatar='" . $value . "' WHERE IDUser=" . $this->id;
-            $db->_query($req);
-            if ($db->affected_rows == 0) {
-                // log d'échec de mise à jour
-                $db_log = new Log("db");
-                $db_log->log("Echec de l'update de l'avatar (" . $this->getIdentite() . ")", Log::ERR);
-            }
-            return $db->affected_rows;
-        } else {    return 1;   }
-    }
-    
-    public function setEmail($value)
-    {
-        if ($this->getEmail() != $value) {
-            if (!self::ctrlMail($value)) {   return -1;  }
-            $db = Registry::get('db');
-            if ($db->count("Utilisateurs", " WHERE Email='" . $value . "'") > 0) {   return -2;  }
-
-            $req = "UPDATE Utilisateurs SET Email='" . $value . "' WHERE IDUser=" . $this->id;
-            $db->_query($req);
-            if ($db->affected_rows == 0) {
-                // log d'échec de mise à jour
-                $db_log = new Log("db");
-                $db_log->log("Echec de l'update de l'email (" . $this->getIdentite() . ")", Log::ERR);
-            }
-            return $db->affected_rows;
-        } else {    return 1;   }
-    }
-    
-    public function setSexe($value)
-    {
-        if (intval($value) != 1 && intval($value) != 2)     {   return -1;   }
-        $value = intval($value)-1;
-        if ($this->getSexe() != $value) {
-            $db = Registry::get('db');
-            $req = "UPDATE Utilisateurs SET Sexe=" . $value . " WHERE IDUser=" . $this->id;
-            $res = $db->_query($req);
-            if ($db->affected_rows == 0) {
-                // log d'échec de mise à jour
-                $db_log = new Log("db");
-                $db_log->log("Echec de l'update du sexe (" . $this->identite . ")", Log::ERR);
-            }
-            return $db->affected_rows;
-        } else {    return 1;   }
-    }
-    
-    public function setLangue($value)
-    {
-        $opt = Registry::get("__options__");
-        $langs = $opt['language']['langs'];
-        $value = htmlentities($value);
-        if ($this->getLangue() != $value) {
-            if (array_key_exists($value, $langs)) {
-                $db = Registry::getInstance();
-                $req = "UPDATE Utilisateurs SET Langue='" . $value . "' WHERE IDUser=" . $this->id;
-                $db->_query($req);
-                if ($db->affected_rows == 0) {
-                    // log d'échec de mise à jour
-                    $db_log = new Log("db");
-                    $db_log->log("Echec de l'update de la langue (" . $this->identite . ")", Log::ERR);
-                } else
-                {
-                    $translate = Registry::get("translate");
-                    $translate->setLangUser($value);
-                }
-                return $db->affected_rows;
-            } else {    return 0;   }
-        } else {    return 1;   }
-    }
-    
-    // Controle de l'identité
-    // ======================
-    public static function ctrlIdentity($identity)
-    {
+    /**
+     * @param string $identity
+     * @return boolean
+     */
+    public function validateIdentity($identity) {
         $res = '';
         preg_match("/^[a-zA-Z][a-zA-Z0-9 _-]+/", $identity, $res);
 
-        if (!$res || $res[0] != $identity)	{   return false;   }
-        if (intval($identity) != 0)             {   return false;   }
-        if (strlen($identity) < 3)              {   return false;   }
-        
+        if (
+            !$res || $res[0] !== $identity ||
+            intval($identity) !== 0 ||
+            strlen($identity) < 3
+        ) {
+            return false;
+        }
         return true;
     }
     
-    
-    // Controle de l'existence du mail
-    // ===============================
-    public static function ctrlMail($email)
-    {
-        $res = ''; $Username = ''; $Domain =''; $MXHost = '';
-        $Modele = "/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/";
-        preg_match($Modele, $email, $res);
+    /**
+     * @param string $email
+     * @return boolean
+     */
+    public function validateEmailAddress($email) {
+        $res = '';
+        $MXHost = '';
+        preg_match("/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/", $email, $res);
         
-        if (!$res || $res[0] != $email)         {   return false;   }
-        
-        list($Username, $Domain) = explode("@",$email);
-        if(!getmxrr($Domain, $MXHost))		{   return false;   }
-        
+        if (!$res || $res[0] !== $email) {
+            return false;
+        }
+        $emailParts = explode('@',$email);
+        if(!getmxrr($emailParts[1], $MXHost)) {
+            return false;
+        }
         return true;
     }
     
-    
-    // Verification si un utilisateur appartient à un groupe
-    // =====================================================
-    public static function EstMembre($Group_id, $User_id=0)
-    {
-        $db = Registry::get('db');
-        $id_user = $User_id;
-        if ($id_user == 0) {
-            $auth = Auth::getInstance();
-            $id_user = $auth->_getIdentity();
+    /**
+     * @param int $groupId
+     * @param int $userId
+     * @return int
+     */
+    public function isMember($groupId, $userId = 0) {
+        if ($userId === 0) {
+            $userId = $this->application->get('auth')->getIdentity();
         }
-
-        return $db->count("Citizen_Groups", sprintf(" WHERE Citizen_id=%d AND Group_id=%d", $id_user, $Group_id));
+        return $this->application->get('mysqli')->count('Citizen_Groups', sprintf('WHERE Citizen_id=%d AND Group_id=%d', $userId, $groupId));
     }
-        
     
-    // Verification si un utilisateur est contact de region
-    // ====================================================
-    public static function isContact($id_group = null)
-    {
-        $db = Registry::get('db');
-        $auth = Auth::getInstance();
-        $id_user = $auth->_getIdentity();
+    /**
+     * @param int $groupId
+     * @return int
+     */
+    public function isContact($groupId = null) {
+        $db = $this->application->get('mysqli');
+        $userId = $this->application->get('auth')->getIdentity();
         
-        if (!isset($id_group)) {
-            $res = $db->count("Groups", " WHERE ID_Contact=" . $id_user);
-        } else {
-            $res = $db->count("Groups", " WHERE ID_Contact=" . $id_user . " AND Group_id=" . $id_group);
-        }
-        
-        return ($res);
+        return
+            (!isset($groupId))
+            ? $db->count('Groups', " WHERE ID_Contact = $userId")
+            : $db->count('Groups', " WHERE ID_Contact = $userId AND Group_id = $groupId")
+        ;
     }
 
-    
-    // Renvoi du nombre total d'inscrits
-    // =================================
-    public static function Nb_Members()
-    {
-        $db = Registry::get('db');
-        return $db->count("Utilisateurs");
+    /**
+     * @return int
+     */
+    public static function countMembers() {
+        return $this->application->get('mysqli')->count('users');
     }
     
-    
-    // Renvoi du nombre d'actifs (dernière connexion < 3 semaines)
-    // ===========================================================
-    public static function Nb_ActivesMembers()
-    {
-        $db = Registry::get('db');
-        return $db->count("Utilisateurs", " WHERE DATEDIFF(CURDATE(), DerConnexion) <21");
+    /**
+     * @return int
+     */
+    public static function countActiveMembers() {
+        return $this->application->get('mysqli')->count('users', ' WHERE DATEDIFF(CURDATE(), last_connected_at) < 21');
     }
     
-    
-    // Renvoi de la liste des pays
-    // ===========================
-    public function listCountries()
-    {
-        $db = Registry::get('db');
-        $code_lang = $this->langue;
-        if ($db->ExistColumn($code_lang, "country") == 0)       {   $code_lang = "en";  }
-
-        $req = "SELECT Code, " . $code_lang . " " .
-               "FROM country";
-        $res = $db->select($req);
-        return $res;
+    /**
+     * Countries should have their own manager
+     * 
+     * @ToRemove
+     * @return array
+     */
+    public function getCountries() {
+        $db = $this->application->get('mysqli');
+        $language = $this->langue;
+        if ($db->ExistColumn($language, 'country') == 0) {
+            $language = 'en';
+        }
+        return $db->select("SELECT Code, $language FROM country");
     }
     
-    
-    // Renvoi de la liste des membres
-    // ==============================
-    public static function ListMembers($params)
-    {
-        $db = Registry::get('db');
-        $search = "";
-        $table = "Utilisateurs";
-        if (isset($params['sel_groups']) && !empty($params['sel_groups'])) {
-            $table = "Citizen_Groups, Utilisateurs";
-            $search = " WHERE Citizen_id=IDUser AND Group_id=" . $params['sel_groups'] . " ";
+    /**
+     * @param array $params
+     * @return array
+     */
+    public function getMembers($params) {
+        $search = '';
+        $table = 'Utilisateurs';
+        if (!empty($params['sel_groups'])) {
+            $table = 'Citizen_Groups, users';
+            $search = " WHERE Citizen_id = id AND Group_id={$params['sel_groups']} ";
         }
         
-        $req = "SELECT iduser, avatar, identite, sexe, email, langue, pays, region, derconnexion, inscription " .
-               "FROM " . $table . " " . $search .
-               "ORDER BY identite ASC";
-        
-        return $db->select($req);
+        return $this->application->get('mysqli')->select(
+            'SELECT id, avatar, identity, gender, email, language, country, region, last_connected_at, created_at ' .
+            "FROM $table $search ORDER BY identity ASC"
+        );
     }
     
-    
-    // Renvoi la liste des Contacts de Groupe
-    // ======================================
-    public static function ListContactsGroups()
-    {
-        $db = Registry::get('db');
-        $req = "SELECT IDUser, Group_name, Identite " .
-               "FROM Utilisateurs, Groups WHERE IDUser=ID_Contact " .
-               "ORDER BY Group_name ASC";
-        
-        return $db->select($req);
+    /**
+     * @return array
+     */
+    public function getContactGroups() {
+        return $this->application->get('mysqli')->select(
+            'SELECT id, Group_name, identity ' .
+            'FROM users, Groups WHERE id = ID_Contact ' .
+            'ORDER BY Group_name ASC'
+        );
     }
 }
