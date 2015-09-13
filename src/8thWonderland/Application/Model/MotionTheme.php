97dc5f04468e65eac9a96cc0d7e4d689d5f79751
Axel Venet <kern046@gmail.com>
2015-09-13 20:04:39 +0200

    Create model for MotionTheme

---
+++ src/8thWonderland/Application/Model/MotionTheme.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Wonderland\Application\Model;
+
+class MotionTheme {
+    /** @var int **/
+    protected $id;
+    /** @var string **/
+    protected $name;
+    /** @var int **/
+    protected $duration;
+    
+    /**
+     * @return int
+     */
+    public function getId() {
+        return $this->id;
+    }
+    
+    /**
+     * @param string $name
+     * @return \Wonderland\Application\Model\MotionTheme
+     */
+    public function setName($name) {
+        $this->name = $name;
+        
+        return $this;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getName() {
+        return $this->name;
+    }
+    
+    /**
+     * @param int $duration
+     * @return \Wonderland\Application\Model\MotionTheme
+     */
+    public function setDuration($duration) {
+        $this->duration = $duration;
+        
+        return $this;
+    }
+    
+    /**
+     * @return int
+     */
+    public function getDuration() {
+        return $this->duration;
+    }
+}
