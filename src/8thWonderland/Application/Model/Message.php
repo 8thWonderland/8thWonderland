f5e3ab2981a49e7b01c1522552a4133cf0943c51
Axel Venet <kern046@gmail.com>
2015-09-22 22:58:32 +0200

    Implement model for messages

--- src/8thWonderland/Application/Model/Message.php
+++ src/8thWonderland/Application/Model/Message.php
@@ -2,100 +2,109 @@
 
 namespace Wonderland\Application\Model;
 
-use Wonderland\Library\Memory\Registry;
-
-use Wonderland\Library\Auth;
-
-/**
- * class managegroups
- *
- * Gestion des informations des groupes
- *
- * @author Brennan
- */
 class Message {
-    
-    // Enregistre un nouveau message
-    // =============================
-    public static function create_message($datas)
-    {
-        $db = Registry::get('db');
-        
-        $title = htmlentities($datas['title_message']);
-        $msg   = htmlentities($datas['content_message']);
-        $auth = Auth::getInstance();
-        $author = $auth->getIdentity();
-        
-        $req = "INSERT INTO messages_received (title, content, author, recipient) VALUES ('" . $title . "', '" . $msg . "', " . $author . ", " . $datas['recipient_message'] . ")";
-        $db->query($req);
-        $req = "INSERT INTO messages_sent (title, content, author, recipients) VALUES ('" . $title . "', '" . $msg . "', " . $author . ", " . $datas['recipient_message'] . ")";
-        $db->query($req);
-        
-        return $db->affected_rows;
-    }
-    
-    
-    // Affichage des messages reçus
-    // ============================
-    public static function display_receivedmessages()
-    {
-        return Registry::get('db')->select(
-            'SELECT id_receivedmessage, title, identite, date_msg ' .
-            'FROM messages_received, Utilisateurs ' .
-            'WHERE author=Utilisateurs.IDUser AND recipient=' . Auth::getInstance()->getIdentity() . ' ' .
-            'ORDER BY date_msg DESC'
-        );
-    }
-        
-    
-    // Affichage des messages envoyés
-    // ==============================
-    public static function display_sentmessages() {
-        return Registry::get('db')->select(
-            'SELECT id_sentmessage, title, recipients, date_msg ' .
-            'FROM messages_sent ' .
-            'WHERE author=' . Auth::getInstance()->getIdentity() . ' ' .
-            'ORDER BY date_msg DESC'
-        );
-    }
+    /** @var int **/
+    protected $id;
+    /** @var string **/
+    protected $title;
+    /** @var string **/
+    protected $content;
+    /** @var string **/
+    protected $author;
+    /** @var \DateTime **/
+    protected $createdAt;
+    /** @var array **/
+    protected $recipients;
     
     /**
-     * @param int $id
-     * @param string $type
-     * @return array
-     */
-    public static function display_contentmessage($id, $type) {
-        $db = Registry::get('db');
-        $auth = Auth::getInstance();
-        
-        $req =
-            ($type === 1)
-            ? 
-                "SELECT title, content, recipients, date_msg " .
-                "FROM messages_sent " .
-                "WHERE author=" . $auth->getIdentity() . " " .
-                "ORDER BY date_msg DESC"
-            :
-                "SELECT title, content, recipient, date_msg " .
-                "FROM messages_received, Utilisateurs " .
-                "WHERE author=Utilisateurs.IDUser AND recipient=" . $auth->getIdentity() . " " .
-                "ORDER BY date_msg DESC"
-        ;
-        return $db->select($req);
-    }
-    
-    /**
-     * @param int $id
-     * @param string $type
      * @return int
      */
-    public static function delete_message($id, $type) {
-        $db = Registry::get('db');
-        $db->query(
-            ($type === 1)
-            ? "DELETE FROM messages_sent WHERE id_sentmessage=$id"
-            : "DELETE FROM messages_received WHERE id_receivedmessage=$id"
-        );
-        return $db->affected_rows;
+    public function getId() {
+        return $this->id;
+    }
+    
+    /**
+     * @param string $title
+     * @return \Wonderland\Application\Model\Message
+     */
+    public function setTitle($title) {
+        $this->title = $title;
+        
+        return $this;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getTitle() {
+        return $this->title;
+    }
+    
+    /**
+     * @param string $content
+     * @return \Wonderland\Application\Model\Message
+     */
+    public function setContent($content) {
+        $this->content = $content;
+        
+        return $this;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getContent() {
+        return $this->content;
+    }
+    
+    /**
+     * @param string $author
+     * @return \Wonderland\Application\Model\Message
+     */
+    public function setAuthor($author) {
+        $this->author = $author;
+        
+        return $this;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getAuthor() {
+        return $this->author;
+    }
+    
+    /**
+     * @param \DateTime $createdAt
+     * @return \Wonderland\Application\Model\Message
+     */
+    public function setCreatedAt(\DateTime $createdAt) {
+        $this->createdAt = $createdAt;
+        
+        return $this;
+    }
+    
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt() {
+        return $this->createdAt;
+    }
+    
+    /**
+     * @param array $recipients
+     * @return \Wonderland\Application\Model\Message
+     */
+    public function setRecipients($recipients) {
+        $this->recipients = $recipients;
+        
+        return $this;
+    }
+    
+    /**
+     * @return array
+     */
+    public function getRecipients() {
+        return $this->recipients;
     }
 }
