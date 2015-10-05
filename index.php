cbffe3e81a688a09a5ebf3af011965a3a875e643
Axel Venet <kern046@gmail.com>
2015-10-05 10:27:24 +0200

    Hotfix that damn path

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 
 require_once 'vendor/autoload.php';
 
-define ( 'CSS_PATH', $_SERVER['REQUEST_URI'] . '/src/8thWonderland/public/css/' );
-define ( 'IMG_PATH', $_SERVER['REQUEST_URI'] . '/src/8thWonderland/public/images/' );
-define ( 'ICO_PATH', $_SERVER['REQUEST_URI'] . '/src/8thWonderland/public/icones/' );
-define ( 'JS_PATH', $_SERVER['REQUEST_URI'] . '/src/8thWonderland/public/js/' );
+ini_set('display_errors', 1);
+
+define ( 'CSS_PATH', $_SERVER['REQUEST_URI'] . 'src/8thWonderland/public/css/' );
+define ( 'IMG_PATH', $_SERVER['REQUEST_URI'] . 'src/8thWonderland/public/images/' );
+define ( 'ICO_PATH', $_SERVER['REQUEST_URI'] . 'src/8thWonderland/public/icones/' );
+define ( 'JS_PATH', $_SERVER['REQUEST_URI'] . 'src/8thWonderland/public/js/' );
 
 $application = new Wonderland\Library\Application();
 $application->init();
