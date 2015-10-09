4acb7fed0df80ee05a01f1f6ff440d548e344675
Axel Venet <kern046@gmail.com>
2015-10-09 11:45:10 +0200

    Fix path definition for ressources

--- index.php
+++ index.php
@@ -4,10 +4,18 @@ require_once 'vendor/autoload.php';
 
 ini_set('display_errors', 1);
 
-define ( 'CSS_PATH', $_SERVER['REQUEST_URI'] . 'src/8thWonderland/public/css/' );
-define ( 'IMG_PATH', $_SERVER['REQUEST_URI'] . 'src/8thWonderland/public/images/' );
-define ( 'ICO_PATH', $_SERVER['REQUEST_URI'] . 'src/8thWonderland/public/icones/' );
-define ( 'JS_PATH', $_SERVER['REQUEST_URI'] . 'src/8thWonderland/public/js/' );
+$rootPath = 
+    (basename($_SERVER['REQUEST_URI']) === 'index.php')
+    ? dirname($_SERVER['REQUEST_URI']) . '/'
+    : $_SERVER['REQUEST_URI']
+;
+
+
+
+define ('CSS_PATH', "{$rootPath}src/8thWonderland/public/css/");
+define ('IMG_PATH', "{$rootPath}src/8thWonderland/public/images/");
+define ('ICO_PATH', "{$rootPath}src/8thWonderland/public/icones/");
+define ('JS_PATH', "{$rootPath}src/8thWonderland/public/js/");
 
 $application = new Wonderland\Library\Application();
 $application->init();
