9dec621ee760db98c5676079b215cb43645a7930
Axel Venet <kern046@gmail.com>
2015-11-04 12:04:39 +0100

    Improve renderer's dependency injection and add methods to handle views parameters

--- src/8thWonderland/Library/Templating/Renderer.php
+++ src/8thWonderland/Library/Templating/Renderer.php
@@ -4,16 +4,49 @@ namespace Wonderland\Library\Templating;
 
 class Renderer {
     /** @var array **/
-    protected $parameters;
+    protected $parameters = [];
+    /** @var string **/
+    protected $rootPath;
+    
+    /**
+     * @param string $rootPath
+     * @param \Wonderland\Library\Templating\Translator $translator
+     */
+    public function __construct($rootPath, Translator $translator) {
+        $this->rootPath = $rootPath;
+        $this->parameters['translator'] = $translator;
+    }
+    
+    /**
+     * @param string $key
+     * @param mixed $value
+     */
+    public function addParameter($key, $value) {
+        $this->parameters[$key] = $value;
+    }
+    
+    /**
+     * @param array $parameters
+     */
+    public function addParameters($parameters) {
+        $this->parameters = array_merge($this->parameters, $parameters);
+    }
+    
+    /**
+     * @param type $key
+     */
+    public function removeParameter($key) {
+        unset($this->parameters[$key]);
+    }
     
     /**
      * @param string $view
      * @param array $parameters
      */
     public function render($view, $parameters = []) {
-        $this->parameters = $parameters;
-        $this->parameters['translator'] = $this->application->get('translator');
-        
+        if(count($parameters) > 0) {
+            $this->addParameters($parameters);
+        }
         require("{$this->rootPath}/Application/views/$view.view");
     }
 }
