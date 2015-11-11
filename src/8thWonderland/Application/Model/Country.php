2179dbbc7db43150301a480d653ae088d3cb6e4a
Axel Venet <kern046@gmail.com>
2015-11-11 15:43:16 +0100

    Add model class for countries

---
+++ src/8thWonderland/Application/Model/Country.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Wonderland\Application\Model;
+
+class Country {
+    /** @var int **/
+    protected $id;
+    /** @var string **/
+    protected $code;
+    /** @var string **/
+    protected $label;
+    
+    /**
+     * @param int $id
+     * @return \Wonderland\Application\Model\Country
+     */
+    public function setId($id) {
+        $this->id = $id;
+        
+        return $this;
+    }
+    
+    /**
+     * @return int
+     */
+    public function getId() {
+        return $this->id;
+    }
+    
+    /**
+     * @param string $code
+     * @return \Wonderland\Application\Model\Country
+     */
+    public function setCode($code) {
+        $this->code = $code;
+        
+        return $this;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getCode() {
+        return $this->code;
+    }
+    
+    /**
+     * @param string $label
+     * @return \Wonderland\Application\Model\Country
+     */
+    public function setLabel($label) {
+        $this->label = $label;
+        
+        return $this;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getLabel() {
+        return $this->label;
+    }
+}
