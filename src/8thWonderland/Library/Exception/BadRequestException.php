e9408a9b607f6b8f7ce8de54f9aefdb799c4e5e1
Axel Venet <kern046@gmail.com>
2015-11-19 09:33:44 +0100

    Add new exception to handle bad requests

---
+++ src/8thWonderland/Library/Exception/BadRequestException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Wonderland\Library\Exception;
+
+class BadRequestException extends AbstractException {
+    public function handle() {
+        header("{$_SERVER['SERVER_PROTOCOL']} 400 Bad Request");
+        header('Content-Type: application/json');
+        
+        echo json_encode([
+            'message' => $this->getMessage()
+        ]);
+    }
+}
