cbf8348dfeb8ae05a3dd51145bc8b328dc93f8a7
Axel Venet <kern046@gmail.com>
2015-12-17 15:58:27 +0100

    Change vote tokens table name and motion model formatting

--- src/8thWonderland/Application/Repository/MotionRepository.php
+++ src/8thWonderland/Application/Repository/MotionRepository.php
@@ -6,13 +6,17 @@ use Wonderland\Application\Model\Member;
 use Wonderland\Application\Model\Motion;
 
 class MotionRepository extends AbstractRepository {
-    
+    /**
+     * @param \Wonderland\Application\Model\Member $member
+     * @param boolean $raw
+     * @return array|Member
+     */
     public function getActiveMotions(Member $member, $raw = true) {
         $statement = $this->connection->prepareStatement(
-            'SELECT m.id, m.title, m.ended_at, COUNT(mvj.*) as has_already_voted ' .
+            'SELECT m.id, m.title, m.created_at, m.ended_at, COUNT(mvt.citizen_id) as has_already_voted ' .
             'FROM motions m ' .
-            'INNER JOIN motions_votes_jetons mvj ON mvj.motion_id = m.id AND mvj.citizen_id = :citizen_id' .
-            'WHERE m.ended_at >  NOW() ' .
+            'INNER JOIN motions_vote_tokens mvt ON mvt.motion_id = m.id AND mvt.citizen_id = :citizen_id ' .
+            'WHERE m.ended_at > NOW() ' .
             'ORDER BY m.ended_at DESC '
         , ['citizen_id' => $member->getId()]);
         
@@ -27,6 +31,10 @@ class MotionRepository extends AbstractRepository {
         return $result;
     }
     
+    /**
+     * @param array $data
+     * @return array
+     */
     public function formatArray($data) {
         $result = [
             'id' => $data['id'],
@@ -42,10 +50,17 @@ class MotionRepository extends AbstractRepository {
         return $result;
     }
     
+    /**
+     * @param array $data
+     * @return Member
+     */
     public function formatObject($data) {
         return
             (new Motion())
             ->setId($data['id'])
+            ->setTitle($data['title'])
+            ->setCreatedAt(new \DateTime($data['created_at']))
+            ->setEndedAt(new \DateTime($data['ended_at']))
         ;
     }
 }
