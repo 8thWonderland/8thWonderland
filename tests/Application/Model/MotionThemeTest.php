fedd94f8982cdc06823736705d0a7c2610ccb345
Axel Venet <kern046@gmail.com>
2015-12-31 14:55:54 +0100

    Fix unit tests about motion theme model

--- tests/Application/Model/MotionThemeTest.php
+++ tests/Application/Model/MotionThemeTest.php
@@ -9,11 +9,11 @@ class MotionThemeTest extends \PHPUnit_Framework_TestCase {
         $motionTheme =
             (new MotionTheme())
             ->setId(1)
-            ->setName('motions.constitutional')
+            ->setLabel('motions.constitutional')
             ->setDuration(8)
         ;
         $this->assertEquals(1, $motionTheme->getId());
-        $this->assertEquals('motions.constitutional', $motionTheme->getName());
+        $this->assertEquals('motions.constitutional', $motionTheme->getLabel());
         $this->assertEquals(8, $motionTheme->getDuration());
     }
 }
