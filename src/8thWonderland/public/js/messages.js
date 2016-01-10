543a00b6eff952e9a99e75dbe7ce696aef055d66
Axel Venet <kern046@gmail.com>
2016-01-10 18:07:30 +0100

    Refactor the inbox view and front logic to display a message content

--- src/8thWonderland/public/js/messages.js
+++ src/8thWonderland/public/js/messages.js
@@ -19,4 +19,58 @@ function reloadMessages(data) {
     });
 }
 
+function readMessage(messageId) {
+    $.ajax({
+        type: 'GET',
+        url: website_root + 'message/read',
+        data: "message_id=" + messageId,
+        dataType: 'json',
+        success: function(data) {
+            markMessageAsRead(messageId);
+            deployMessage(data.message);
+        }
+    });
+}
+
+function markMessageAsRead(messageId) {
+    var messageItem = $("#message-" + messageId + " a");
+    
+    if(messageItem.attr('class') === 'message-unread') {
+        messageItem.attr('class', '');
+        updateUnreadMessagesCounter();
+    }
+}
+
+function updateUnreadMessagesCounter() {
+    var messageCounter = $("#messages span");
+    var nbUnreadMessages = parseInt(messageCounter.text()) - 1;
+    
+    if(nbUnreadMessages < 1) {
+        messageCounter.attr('class', 'badge');
+    }
+    messageCounter.text(nbUnreadMessages);
+}
+
+function deployMessage(message) {
+    removeMessage();
+    
+    $("#message-area")
+        .prepend(
+            '<section id="message-details-' + message.id + '" class="message-details">' +
+            '<header><h3>' + message.title + '</h3> ' +
+            '<div onclick="removeMessage();return false;"><i class="fa fa-close fa-2x"></i></div></header>' +
+            '<p>' + message.content + '</p>' + 
+            '</section>'
+        )
+        .find('.message-details')
+        .animate({width: "100%"}, 600)
+    ;
+}
+
+function removeMessage() {
+    $(".message-details").animate({width: "0%"}, 600, function() {
+        $(this).remove();
+    });
+}
+
 $(".paginated-list").paginate();
