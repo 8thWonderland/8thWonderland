60d930635acf751ae4335ad36dbd05a1396dbd75
Axel Venet <kern046@gmail.com>
2016-01-22 12:50:07 +0100

    Remove old IndexController methods

--- src/8thWonderland/Application/Controller/IndexController.php
+++ src/8thWonderland/Application/Controller/IndexController.php
@@ -22,45 +22,4 @@ class IndexController extends ActionController
             'countries' => $this->application->get('country_manager')->getCountries(),
         ]);
     }
-
-    public function presentationAction()
-    {
-        return $this->render('informations/presentation');
-    }
-
-    public function subscribeAction()
-    {
-        $translate = $this->application->get('translator');
-        $langs = $translate->getList();
-        $sel_lang = '';
-        $nbLangs = count($langs);
-        for ($i = 0; $i < $nbLangs; ++$i) {
-            $sel_lang .= "<option value='{$langs[$i]}'>{$translate->translate($langs[$i])}</option>";
-        }
-
-        return $this->render('members/subscribe', [
-            'langs' => $sel_lang,
-        ]);
-    }
-
-    public function partnersAction()
-    {
-        return $this->render('informations/partners');
-    }
-
-    public function newsAction()
-    {
-        $facebookManager = $this->application->get('facebook_manager');
-
-        return $this->render('informations/public_news', [
-            'facebook_feed' => $facebookManager->getPageFeed(3),
-            'facebook_picture' => $facebookManager->getPagePicture(),
-            'facebook_page' => $facebookManager->getPageInformations(),
-        ]);
-    }
-
-    public function contactAction()
-    {
-        return $this->render('communications/contactus');
-    }
 }
